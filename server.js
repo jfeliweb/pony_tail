c147d8c08cafd8113bf6a16c3c88bc8736bfe602
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-11-18 12:26:11 -0500

    add mongoose, connect mongodb atlas, add colors

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-// Route files
-const salons = require('./routes/salons');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load ENV 
 dotenv.config({
     path: './config/config.env'
 });
+
+// Connect to Mongoose
+connectDB();
+
+// Route files
+const salons = require('./routes/salons');
 
 const app = express();
 
@@ -22,6 +28,13 @@ app.use('/api/v1/salons', salons);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log(`Server up and running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+    console.log(`Server up and running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold.underline);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red.underline);
+    // Close server & exit process
+    server.close(() => process.exit(1));
 });

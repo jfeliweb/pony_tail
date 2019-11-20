0a67c31f9857ebe9a3094460c80255a7aa213847
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-11-20 08:22:39 -0500

    add Async Handler

--- controllers/salons.js
+++ controllers/salons.js
@@ -1,11 +1,11 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Salon = require('../models/Salon');
 
 // @desc    Get all salons
 // @route   GET /api/v1/salons
 // @access  Public
-exports.getSalons = async (req, res, next) => {
-    try {  
+exports.getSalons = asyncHandler(async (req, res, next) => {
         const salons = await Salon.find();
 
         res.status(200).json({
@@ -13,17 +13,12 @@ exports.getSalons = async (req, res, next) => {
             count: salons.length,
             data: salons
         });
-    } catch (err) {
-        next(err);
-    }
-    
-};
+});
 
 // @desc    Get single salons
 // @route   GET /api/v1/salons/:id
 // @access  Public
-exports.getSalon = async (req, res, next) => {
-    try {
+exports.getSalon = asyncHandler(async (req, res, next) => {
     const salon = await Salon.findById(req.params.id);
 
     if (!salon) {
@@ -34,35 +29,25 @@ exports.getSalon = async (req, res, next) => {
     res.status(200).json({
         success: true,
         data: salon
-    })
-    } catch (err) {
-        // The id format is not correct
-        next(err);
-    }
-};
+    });
+});
 
 // @desc    Create new salon
 // @route   POST /api/v1/salons
 // @access  Private
-exports.createSalon = async (req, res, next) => {
-    try {
+exports.createSalon = asyncHandler(async (req, res, next) => {
         const salon = await Salon.create(req.body);
 
         res.status(201).json({
             success: true,
             data: salon
         });
-    } catch (err) {
-        next(err);
-    }
-    
-};
+});
 
 // @desc    Update salon
 // @route   PUT /api/v1/salons/:id
 // @access  Private
-exports.updateSalon = async (req, res, next) => {
-    try {
+exports.updateSalon = asyncHandler(async (req, res, next) => {
         const salon = await Salon.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
@@ -76,18 +61,12 @@ exports.updateSalon = async (req, res, next) => {
             success: true,
             data: salon
         });
-
-    } catch (err) {
-        next(err);
-    }
-    
-};
+});
 
 // @desc    Delete salon
 // @route   DELETE /api/v1/salons/:id
 // @access  Private
-exports.deleteSalon = async (req, res, next) => {
-    try {
+exports.deleteSalon = asyncHandler(async (req, res, next) => {
         const salon = await Salon.findByIdAndDelete(req.params.id);
 
         if (!salon) {
@@ -98,8 +77,4 @@ exports.deleteSalon = async (req, res, next) => {
             success: true,
             data: {}
         });
-
-    } catch (err) {
-        next(err);
-    }
-};
+});

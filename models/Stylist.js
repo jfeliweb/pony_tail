3dd2081e9b12fb8940d9b00c690b6aa5fc339fd9
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-11-24 18:28:36 -0500

    add average cost funtionality

--- models/Stylist.js
+++ models/Stylist.js
@@ -55,4 +55,41 @@ const StylistSchema = new mongoose.Schema({
     }
 });
 
+// Static method to get avg of course tuitions
+StylistSchema.statics.getAverageCost = async function  (salonId) {
+    console.log('Calculating avg cost...'.blue);
+
+    const obj = await this.aggregate([
+        {
+            $match: { salon: salonId }
+        },
+        {
+            $group: {
+                _id: '$salon',
+                averageCost: {
+                    $avg: '$hourlyRate'
+                }
+            }
+        }
+    ]);
+
+    try {
+        await this.model('Salon').findByIdAndUpdate(salonId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Call getAverageCost after save
+StylistSchema.post('save', function () { 
+    this.constructor.getAverageCost(this.salon);
+ });
+
+ // Call getAverageCost before remove
+ StylistSchema.pre('save', function () {
+    this.constructor.getAverageCost(this.salon);
+ });
+
 module.exports = mongoose.model('Stylist', StylistSchema);

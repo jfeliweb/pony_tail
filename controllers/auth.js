91d8bc0845b11363dec5907ed1f303d5af06d806
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-12-05 21:16:22 -0500

    setup cookie to store token

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,10 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     });
 
-    // Create Token
-    const token = user.getSignedJwtToken();
-
-    res.status(200).json({ success: true, token });
+    sendTokenResponse(user, 200, res);
 });
 
 // @desc    Login User
@@ -50,8 +47,28 @@ exports.login = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse('Invalid credentials', 401));
     }
 
+    sendTokenResponse(user, 200, res);
+});
+
+// Get token from Model & create Cookie  and sen Response
+const sendTokenResponse = (user, statusCode, res) => {
     // Create Token
     const token = user.getSignedJwtToken();
 
-    res.status(200).json({ success: true, token });
-});
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true;
+    }
+
+    res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({ 
+        success:true,
+        token
+    });
+}
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -24,6 +25,9 @@ const app = express();
 
 // Body parser
 app.use(express.json());
+
+// Cookie Parser
+app.use(cookieParser());
 
 // DEV logging middleware
 if (process.env.NODE_ENV === 'development') {

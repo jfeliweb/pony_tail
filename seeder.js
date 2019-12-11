27bcd266430d98a4df41e5a2f4f0474020377950
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-12-11 10:48:28 -0500

    update seeder to add reviews

--- seeder.js
+++ seeder.js
@@ -10,50 +10,62 @@ dotenv.config({ path: './config/config.env' });
 const Salon = require('./models/Salon');
 const Stylist = require('./models/Stylist');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
 });
 
 // Read JSON Files
-const salons = JSON.parse(fs.readFileSync(`${__dirname}/_data/salons.json`, 'utf-8'));
-const stylist = JSON.parse(fs.readFileSync(`${__dirname}/_data/stylist.json`, 'utf-8'));
-const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
+const salons = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/salons.json`, 'utf-8')
+);
+const stylist = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/stylist.json`, 'utf-8')
+);
+const users = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+const reviews = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+);
 
 // Import into Database
 const importData = async () => {
-    try {
-        await Salon.create(salons);
-        await Stylist.create(stylist);
-        await User.create(users);
+	try {
+		await Salon.create(salons);
+		await Stylist.create(stylist);
+		await User.create(users);
+		await Review.create(reviews);
 
-        console.log('Data was Imported...'.green.inverse);
-        process.exit();
-    } catch (err) {
-        console.log(err);
-    }
-}
+		console.log('Data was Imported...'.green.inverse);
+		process.exit();
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 // Delete Data from Database
 const deleteData = async () => {
-    try {
-        await Salon.deleteMany();
-        await Stylist.deleteMany();
-        await User.deleteMany();
+	try {
+		await Salon.deleteMany();
+		await Stylist.deleteMany();
+		await User.deleteMany();
+		await Review.deleteMany();
 
-        console.log('Data go down the hole...'.red.inverse);
-        process.exit();
-    } catch (err) {
-        console.log(err);
-    }
-}
+		console.log('Data go down the hole...'.red.inverse);
+		process.exit();
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 if (process.argv[2] === '-i') {
-    importData();
+	importData();
 } else if (process.argv[2] === '-d') {
-    deleteData();
+	deleteData();
 }

61bd39b3b13c1e3615a54f57a26c8b0fe358dd36
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-12-14 12:35:28 -0500

    add & setup xss clean package

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -46,6 +47,9 @@ app.use(mongoSanitize());
 
 // Set Headers Security
 app.use(helmet());
+
+// Prevent Cross-Site Scripting (XSS) Attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));

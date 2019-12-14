9f1b021ba2bc9cdf932267eebaf3b3a6dc9a33b7
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-12-14 12:50:37 -0500

    add & setup rate limit and HTTP Pollution packages

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -50,6 +52,17 @@ app.use(helmet());
 
 // Prevent Cross-Site Scripting (XSS) Attacks
 app.use(xss());
+
+// Rate Limiting
+const apiLimiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 10 minutes
+	max: 100,
+});
+
+app.use(apiLimiter);
+
+// Prevent http param pollution
+app.use(hpp());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));

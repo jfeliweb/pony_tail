ca35c1d9e32679d39fab27c3401887bca0c21b47
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-12-14 12:14:36 -0500

    fix: Simplified error message

--- middleware/error.js
+++ middleware/error.js
@@ -1,35 +1,35 @@
 const ErrorResponse = require('../utils/errorResponse');
 
 const errorHandler = (err, req, res, next) => {
-    let error = { ...err };
-    error.message = err.message;
+	let error = { ...err };
+	error.message = err.message;
 
-    // Log to console for dev
-    console.log(err);
-    console.log(err.stack.red);
+	// Log to console for dev
+	console.log(err);
+	console.log(err.stack.red);
 
-    // Mongoose Bad ObjectId
-    if (err.name === 'CastError') {
-        const message = `Resource not found with id of ${err.value}`;
-        error = new ErrorResponse(message, 404);
-    }
+	// Mongoose Bad ObjectId
+	if (err.name === 'CastError') {
+		const message = `Resource Not Found`;
+		error = new ErrorResponse(message, 404);
+	}
 
-    // Mongoose Duplicate Key
-    if (err.code === 11000) {
-        const message = `Duplicate field value entered`;
-        error = new ErrorResponse(message, 400);
-    }
+	// Mongoose Duplicate Key
+	if (err.code === 11000) {
+		const message = `Duplicate field value entered`;
+		error = new ErrorResponse(message, 400);
+	}
 
-    // Mongoose Validation Error
-    if (err.name === 'ValidationError') {
-        const message = Object.values(err.errors).map(val => val.message);
-        error = new ErrorResponse(message, 400);
-    }
+	// Mongoose Validation Error
+	if (err.name === 'ValidationError') {
+		const message = Object.values(err.errors).map(val => val.message);
+		error = new ErrorResponse(message, 400);
+	}
 
-    res.status(error.statusCode || 500).json({
-        success: false,
-        error: error.message || 'Server Error'
-    });
+	res.status(error.statusCode || 500).json({
+		success: false,
+		error: error.message || 'Server Error',
+	});
 };
 
 module.exports = errorHandler;

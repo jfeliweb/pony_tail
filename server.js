32641960a50b62ca83eadff6cd864628c3489b82
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-12-14 12:13:29 -0500

    add & setup express mongoose sanitize package

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -38,6 +39,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // File Uploading
 app.use(fileupload());
+
+// Sanitize Data
+app.use(mongoSanitize());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
